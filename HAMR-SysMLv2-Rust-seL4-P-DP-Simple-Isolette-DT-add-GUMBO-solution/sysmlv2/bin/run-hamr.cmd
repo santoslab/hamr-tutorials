a8223dfa53ce1b67518bb071dadf0c3b6d148911
Jason Belt <belt@ksu.edu>
2026-06-05 15:32:32 -0500

    add rust examples to ci

---
+++ HAMR-SysMLv2-Rust-seL4-P-DP-Simple-Isolette-DT-add-GUMBO-solution/sysmlv2/bin/run-hamr.cmd
@@ -0,0 +1,59 @@
+::/*#! 2> /dev/null                                 #
+@ 2>/dev/null # 2>nul & echo off & goto BOF         #
+if [ -z ${SIREUM_HOME} ]; then                      #
+  echo "Please set SIREUM_HOME env var"             #
+  exit -1                                           #
+fi                                                  #
+exec ${SIREUM_HOME}/bin/sireum slang run "$0" "$@"  #
+:BOF
+setlocal
+if not defined SIREUM_HOME (
+  echo Please set SIREUM_HOME env var
+  exit /B -1
+)
+%SIREUM_HOME%\\bin\\sireum.bat slang run "%0" %*
+exit /B %errorlevel%
+::!#*/
+// #Sireum
+
+import org.sireum._
+
+val sysmlDir: Os.Path = Os.slashDir.up
+val hamrDir: Os.Path = sysmlDir.up / "hamr"
+
+val sireumBin: Os.Path = Os.path(Os.env("SIREUM_HOME").get) / "bin"
+val sireum: Os.Path = sireumBin / (if(Os.isWin) "sireum.bat" else "sireum")
+
+val packageName: String = "isolette"
+
+var sourcePath: String = sysmlDir.string
+
+assert ((sysmlDir / "aadl-lib").exists, s"${sysmlDir / "aadl-lib"} not present")
+
+var codegenArgs: ISZ[String] = ISZ(
+  sireum.value, "hamr", "sysml", "codegen",
+  "--package-name", packageName,
+  "--output-dir", hamrDir.value,
+  "--verbose",
+  "--no-proyek-ive",
+  "--workspace-root-dir", sysmlDir.string,
+  "--sourcepath", sourcePath,
+  "--system-name", "Isolette::Isolette_System",
+)
+
+codegenArgs = codegenArgs ++ Os.cliArgs
+
+codegenArgs = codegenArgs :+ (sysmlDir / "isolette-simple" / "Isolette.sysml").value
+
+val results = Os.proc(codegenArgs).echo.console.run()
+
+// Running under windows results in 23 which is an indication 
+// a platform restart was requested. Codegen completes 
+// successfully and the cli app returns 0 so 
+// not sure why this is being issued.
+if(results.exitCode == 0 || results.exitCode == 23) {
+  Os.exit(0)
+} else {
+  println(results.err)
+  Os.exit(results.exitCode)
+}
--- bin/build.cmd
+++ bin/build.cmd
@@ -8,13 +8,13 @@ if [ -z ${SIREUM_HOME} ]; then                                #
   echo "Please set SIREUM_HOME env var"                       #
   exit -1                                                     #
 fi                                                            #
-exec ${SIREUM_HOME}/bin/sireum slang run -n "$0" "$@"         #
+exec ${SIREUM_HOME}/bin/sireum slang run "$0" "$@"         #
 :BOF
 if not defined SIREUM_HOME (
   echo Please set SIREUM_HOME env var
   exit /B -1
 )
-%SIREUM_HOME%\bin\sireum.bat slang run -n "%0" %*
+%SIREUM_HOME%\bin\sireum.bat slang run "%0" %*
 exit /B %errorlevel%
 ::!#*/
 // #Sireum
@@ -31,14 +31,12 @@ val sireum: Os.Path = sireumHome / "bin" / (if (Os.isWin) "sireum.bat" else "sir
 val appDir: Os.Path = sireumHome / "bin" / (if (Os.isMac) "mac" else if (Os.isWin) "win" else "linux")
 val codegenHome = sireumHome / "hamr" / "codegen"
 
-val osateDir = {
+val osateDir: Os.Path = {
   Os.env("OSATE_HOME") match {
     case Some(s) => Os.path(s)
     case _ => appDir / s"osate${if (Os.isMac) ".app" else ""}"
   }
 }
-
-installOsateGumbo()
 
 val buildEnv: ISZ[(String, String)] = ISZ(
   ("SIREUM_HOME", sireumHome.string),
@@ -48,12 +46,20 @@ val projects = ISZ(
   "HAMR-Slang-Tutorials-Example-00",
   "HAMR-Slang-Tutorials-Example-00-AADL-Refactored",
   "HAMR-Slang-Tutorials-Prod-Cons",
-  "HAMR-Slang-Tutorials-Prod-Cons-AADL-only"
+  "HAMR-Slang-Tutorials-Prod-Cons-AADL-only",
+
+  "HAMR-SysMLv2-Rust-seL4-P-DP-Example",
+  "HAMR-SysMLv2-Rust-seL4-P-DP-Simple-Isolette-add-DT-solution",
+  "HAMR-SysMLv2-Rust-seL4-P-DP-Simple-Isolette-DT-add-GUMBO-solution"
 )
 for (p <- projects) {
   val root = home / p
   if ((root / "aadl" / "bin").exists) {
-    val ret = buildProject(root)
+    val ret = buildAadlProject(root)
+    assert(ret)
+  }
+  if ((root / "sysmlv2" / "bin").exists) {
+    val ret = buildSysmlProject(root)
     assert(ret)
   }
 }
@@ -78,7 +84,7 @@ if (Os.isLinux) {
       val branch = Os.env("branch_name").get
       proc"git checkout -b $branch".at(home).runCheck()
       proc"git add $home".at(home).runCheck()
-      Os.proc(ISZ[String]("git", "commit", "-m", "GITHUB ACTIONS: Updating repo due to change detection.  See commit diff for mor info")).at(home).runCheck()
+      Os.proc(ISZ[String]("git", "commit", "-m", "GITHUB ACTIONS: Updating repo due to change detection.  See commit diff for more info")).at(home).runCheck()
       proc"git push --set-upstream origin $branch".at(home).runCheck()
     } else {
       println(
@@ -93,7 +99,76 @@ if (Os.isLinux) {
 
 
 object Helper {
-  def buildProject(root: Os.Path): B = {
+  def removeBuildArtifacts(d: Os.Path): Unit = {
+    val removeNames = ops.ISZOps(ISZ("build", "out", "target"))
+    val removeDirs = Os.Path.walk(d, T, F, p => p.isDir && removeNames.contains(p.name))
+    for (d <- removeDirs) {
+      d.removeAll()
+    }
+  }
+  
+  def run(title: String, verboseArg: B, proc: OsProto.Proc): Z = {
+    println(s"$title ...")
+    val r = (if (verboseArg) proc.console.echo else proc).run()
+    if (!r.ok) {
+      println(s"$title failed!")
+      cprintln(F, r.out)
+      cprintln(T, r.err)
+    }
+    return r.exitCode
+  }
+
+  def buildSysmlProject(root: Os.Path): B = {
+    if (Os.env("MICROKIT_SDK").isEmpty) {
+      println("MICROKIT_SDK environment variable not set")
+      return F
+    }
+
+    println(s"Processing $root")
+
+    // update sysml aadl libraries
+    val sysmlv2Dir = root / "sysmlv2"
+    val aadlLibsDir = sysmlv2Dir / "aadl-lib"
+    val sysmlAadlLibsDir: Os.Path = Os.env("SYSML_AADL_LIBRARIES") match {
+      case Some(s) => Os.path(s)
+      case _ =>
+        println("SYSML_AADL_LIBRARIES environement variable is not set")
+        return F
+    }
+    aadlLibsDir.removeAll()
+    sysmlAadlLibsDir.copyOverTo(aadlLibsDir)
+    (aadlLibsDir / ".git").removeAll()
+    (aadlLibsDir / ".gitattributes").removeAll()
+    (aadlLibsDir / ".gitignore").removeAll()
+
+    val hamrDir = root / "hamr"
+    val microkitDir = hamrDir / "microkit"
+
+    var ret = run(s"Cleaning $microkitDir", F, proc"sireum slang run ${sysmlv2Dir / "bin" / "clean.cmd"} $microkitDir")
+
+    if (ret == 0) {
+      ret = run(s"Running HAMR codegen", F, proc"sireum slang run ${sysmlv2Dir / "bin" / "run-hamr.cmd"} --platform Microkit".at(sysmlv2Dir / "isolette-simple"))
+    }
+
+    if (ret == 0) {
+      ret = run("Building the image", F, proc"make RUST_MAKE_TARGET=build-release".at(microkitDir))
+
+      if (ret == 0) {
+        val thermCrateDir = microkitDir / "crates" / "thermostat_thermostat"
+        ret = run("Verifying thermostat", F, proc"make verus".at(thermCrateDir))
+      }
+
+      if (ret == 0) {
+        ret = run("Running the microkit unit tests", F, proc"make test".at(microkitDir))
+      }
+
+      removeBuildArtifacts(microkitDir)
+    }
+
+    return ret == 0
+  }
+
+  def buildAadlProject(root: Os.Path): B = {
     println(s"Processing $root")
     def run(cmd: String): B = {
       val results = proc"$cmd".env(buildEnv).at(root).echo.run()
@@ -150,35 +225,4 @@ object Helper {
     return Os.env("GITLAB_CI").nonEmpty || Os.env("GITHUB_ACTIONS").nonEmpty || Os.env("BUILD_ID").nonEmpty
   }
 
-  def installOsateGumbo(): B = {
-    val versions = (codegenHome / "jvm" / "src" / "main" / "resources" / "phantom_versions.properties").properties
-
-    val hamrJar = s"org.sireum.aadl.osate.hamr_${versions.get("org.sireum.aadl.osate.plugins.version_alt").get}.jar"
-    val gumboJar = s"org.sireum.aadl.gumbo_${versions.get("org.sireum.aadl.gumbo.plugins.version_alt").get}.jar"
-
-    val pluginsDir: Os.Path =
-      if (Os.isMac) osateDir / "Contents" / "Eclipse" / "plugins"
-      else osateDir / "plugins"
-
-    var alreadyInstalled = F
-    if (pluginsDir.exists) {
-      val files = ops.ISZOps(pluginsDir.list.map((p: Os.Path) => p.name))
-      alreadyInstalled = files.contains(hamrJar) && files.contains(gumboJar)
-    }
-
-    if (alreadyInstalled) {
-      println(s"OSATE already up to date: $osateDir\n")
-      return T
-    } else {
-      println("Installing Sireum plugins into OSATE, this will take a while ...")
-      val result = proc"$sireum hamr phantom -u -o ${osateDir.value}".console.run()
-      if (result.ok) {
-        println(s"OSATE installed at ${osateDir}")
-      } else {
-        eprintln(result.err)
-      }
-      println()
-      return result.ok
-    }
-  }
 }
